aa441d855c9ba0d7bc852824654299b151ef32a2
david vu <davidvu@davids-MacBook-Pro.local>
2018-03-21 15:16:08 -0600

    add smooth scrolling to div

--- public/js/main.js
+++ public/js/main.js
@@ -11,18 +11,27 @@ $( document ).ready(function(){
 	});
 	feed.run();
 
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
 
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
 
+      // Store hash
+      var hash = this.hash;
 
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      var target = $(hash).offset().top -100;
 
-
-
-
-
-
-
-
-
+      $('html, body').animate({
+        scrollTop: target
+      }, 1000);
+    } // End if
+  });
 
 
 });
+
